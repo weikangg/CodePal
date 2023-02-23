a156aa26232097e80f4101e68bfde18a9c7c2733
Wei Kang <chongweikang5@gmail.com>
2023-02-24 01:49:52 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -207,7 +207,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://codepal.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
